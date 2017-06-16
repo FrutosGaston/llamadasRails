a87edba6184deb951aa94bad239b0665d4ab9eba
gaston <frutos.gaston@gmail.com>
2017-06-16 14:05:10 -0300

    Cambio a postgresql

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,16 +1,16 @@
 FactoryGirl.define do
   factory :call, class: Call do
-    origin Location.new({pais: 'US', ciudad: 'NY'})
-    destination Location.new({pais: 'US', ciudad: 'Miami'})
-    last 2
-    when_was DateTime.new(2016,11,30,11,1,1,'+0')
+    origin { Location.new({pais: 'US', ciudad: 'NY'}) }
+    destination { Location.new({pais: 'US', ciudad: 'Miami'}) }
+    last { 2 }
+    when_was { DateTime.new(2016,11,30,11,1,1,'+0') }
   end
 end
 
 FactoryGirl.define do
   factory :location, class: Location do
-    pais 'US'
-    ciudad 'Miami'
+    pais { 'US' }
+    ciudad { 'Miami' }
   end
 end
 
